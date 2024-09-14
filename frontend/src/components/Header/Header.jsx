14001f7186ec2ab063f2c0370a21aeca1dc6ac65
tarikul12 <tarikulnishat21@gmail.com>
2024-09-15 00:10:04 +0600

    last file update

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 
 import React from 'react'
 import "./Header.css";
+
 function Header() {
   return (
     <div className='header'>
         <div className="header-contents">
             <h2>Order your faviourt food here</h2>
-            <p>paragraph is defined as “a group of sentences or a single sentence that forms a unit”  </p>
+            <p>Paragraph is defined as “a group of sentences or a single sentence that forms a unit”  </p>
             <button>View Menu</button>
         </div>
       
